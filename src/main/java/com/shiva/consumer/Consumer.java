4725a2f19ae78f6db6f9a1b79de07fdd4cb96a16
shiva <shiva@pgi-amarendrad1.Prodigy.local>
2019-03-08 15:59:23 +0530

    asdasd

--- src/main/java/com/shiva/consumer/Consumer.java
+++ src/main/java/com/shiva/consumer/Consumer.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 public class Consumer {
 @JmsListener(destination="In-memmory-queue")
 public void  myConsumerMethod(String message) {
-System.out.println("Active MQ message:"+message);	
+System.out.println("Active MQ message:"+message);
+//new section
 }
 }
